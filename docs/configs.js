54cf873c967aeece5250b5835853ccde9b02f0be
Yuri Zhang <zhangyong0604@126.com>
2018-11-15 14:41:29 +0800

    remove

--- docs/configs.js
+++ docs/configs.js
@@ -1,5 +1,5 @@
 // var __DomainName='http://10.10.30.170:8182/xdm-web';  // SST
-var __DomainName="http://8.m.sit.tuniu.org/xdm"; //SIT
+var __DomainName="http://8.m.sit.test.org/xdm"; //SIT
 // var __DomainName="https://api.5jyq.com/tnp"; //product
 window["SMALL_BLACK_CONFIG"]={
     "_DomainName":__DomainName,
